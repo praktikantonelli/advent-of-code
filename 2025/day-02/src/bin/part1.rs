9bc7ba455150fe669f228fa038b1b25b011edc12
Luca <luca.antonelli@gmx.ch>
2025-12-03 06:45:48 +0100

    feat(day-02): finished day 2

--- 2025/day-02/src/bin/part1.rs
+++ 2025/day-02/src/bin/part1.rs
@@ -1,19 +1,26 @@
 use itertools::Itertools;
+use pcre2::bytes::Regex;
 
 fn main() {
-    let input = include_str!("../../../../advent-of-code-input/2025/day-02.txt");
+    let input = include_str!("../../../../advent-of-code-input/2025/day-02.txt").trim();
     let output = part1(input);
     println!("{output}");
 }
 
-fn part1(input: &str) -> u32 {
-    let mut sum = 0;
+fn part1(input: &str) -> u64 {
+    let mut sum: u64 = 0;
+    let re = Regex::new(r"^([0-9]+)\1$").unwrap();
     for range in input.split(",") {
+        dbg!(range);
         let (min, max) = range.split("-").collect_tuple().unwrap();
-        let min = min.parse::<u32>().unwrap();
-        let max = max.parse::<u32>().unwrap();
+        let min = min.parse::<u64>().unwrap();
+        let max = max.parse::<u64>().unwrap();
         for number in min..=max {
-            println!("{number}");
+            let number_str = number.to_string();
+            if re.is_match(number_str.as_bytes()).unwrap() {
+                println!("{number}");
+                sum += number;
+            }
         }
     }
     sum
@@ -28,6 +35,6 @@ mod test {
         let result = part1(
             "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
         );
-        assert_eq!(result, 1337775554);
+        assert_eq!(result, 1227775554);
     }
 }
